fca745ec3cd6251a4263a4db2b3f50ea47a85529
sirajju <84080512+sirajju@users.noreply.github.com>
2023-03-29 08:14:53 +0530

    Update install_update.sh

--- install_update.sh
+++ install_update.sh
@@ -2,5 +2,4 @@ echo "Installing update.."
 mv Cryptography.py setup.bat version.txt README.txt -r ..
 cd ..
 rmdir --ignore-fail-on-non-empty Cryptography 
-
-echo "Updated to latest version.."
+echo "\n\nSome problem persist you have to check manually for the latest version downloaded or not\n\nYou can check the new folder name to conform it\n\nThank you for your patience"
